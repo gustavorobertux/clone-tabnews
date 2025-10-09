07cbadad80bdbcc972d74648923c77e036379dbe
Gustavo Roberto Rodrigues Gonçalves <38466612+gustavorobertux@users.noreply.github.com>
2025-10-09 13:06:03 +0000

    retestando o prettier

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,11 @@ function Home() {
   return <h1>Hack the planet!</h1>;
 }
 
+function test() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identacao errada");
+}
 export default Home;
